d86eb0f52157673201f49900aaeeaa94bf0549f2
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-09-04 11:53:41 +0200

    TODO: Timing script

---
+++ scripts/time.py
@@ -0,0 +1,3 @@
+# Small script to test the performance of two or more commands (just wall clock time)
+# NOTE: It does _not_ pretend to be any clever at all, so better specify all absolute
+# paths so that python subprocess.check_call can execute all binaries
