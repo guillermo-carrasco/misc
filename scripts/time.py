e042d3498194fde6f30b763623639c30cbda65d7
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-09-08 17:20:04 +0200

    Benchmarking script working - First basic version

--- scripts/time.py
+++
@@ -1,3 +0,0 @@
-# Small script to test the performance of two or more commands (just wall clock time)
-# NOTE: It does _not_ pretend to be any clever at all, so better specify all absolute
-# paths so that python subprocess.check_call can execute all binaries
